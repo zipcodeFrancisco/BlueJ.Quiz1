d334a9a2bc87b19fd8b2ece54f9632645fc3fbaf
zipcodeFrancisco <zipcodefrancisco@gmail.com>
2019-02-11 11:34:51 -0500

    Ver 2.0

--- LoopFun.java
+++ LoopFun.java
@@ -1,4 +1,5 @@
- 
+
+import java.util.stream.IntStream; 
 
 public class LoopFun
 {
@@ -10,7 +11,12 @@ public class LoopFun
        * @return the factorial of the number
        */
       public Integer factorial(Integer number){
-          return null;
+          Integer container = 1;
+          for(int i = 1; i <= number; i++ ){
+              //System.out.println( i + "" );
+              container = i*container;
+          }
+          return container;
       }
 
       /**
@@ -21,7 +27,13 @@ public class LoopFun
        * @return Upper case string of the first letter of each word
        */
       public String acronym(String phrase) {
-          return null;
+          String[] arrayStr = phrase.split(" ");
+          String container = "";
+          for(int i = 0; i < arrayStr.length; i++){
+              container += arrayStr[i].substring(0, 1).toUpperCase();
+          }
+          
+          return container;
       }
 
       /**
@@ -37,6 +49,24 @@ public class LoopFun
        * @return the encrypted string by shifting each character by three character
        */
       public String encrypt(String word) {
-          return null;
+          //System.out.print('\u000C');
+          //System.out.println(word);
+          //String[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+          char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+          char[] arrayChar = word.toCharArray();
+          Integer lengthArr = alphabet.length;
+          String container = "";
+          //System.out.println( lengthArr );
+          for( char c : arrayChar){
+              Integer indexChar = new String(alphabet).indexOf(c);
+              //System.out.println(indexChar);
+              Integer indexUp = ((indexChar + 3)%lengthArr);
+              //System.out.println( " --> " +  indexUp + " - " + alphabet[indexUp]);
+              container += alphabet[indexUp];
+              //System.out.println( c + "-" + findIndex(alphabet,'b'));
+          }
+          //System.out.println(container);
+          return container;
       }
+      
 }
